74b7a06fe09d908b2c0d390ec4f1063cc240d2fe
isaiah <isaiah.v@comcast.net>
2025-11-23 18:30:27 -0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("org.ivcode.gradle-publish") version "0.1-SNAPSHOT"
+}
+
+group = "org.ivcode"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.ivcode.beeboop
+
+fun main() {
+
+}
